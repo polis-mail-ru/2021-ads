0adadca26eef13fbb6507f35bdeba562c987194d
Komissarov Gleb <gl.k@aol.com>
2021-09-23 10:20:19 +0300

    1

---
+++ src/main/java/ru/mail/polis/ads/part1/alltheshamefeelsthesame/Task1.java
@@ -0,0 +1,12 @@
+package ru.mail.polis.ads.part1.alltheshamefeelsthesame;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(Math.abs(scanner.nextInt() % 10));
+    }
+
+}
