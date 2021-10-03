a14cdfb7fc000e1e5d6cde6245d5f4f1077e376d
mariohuq <mariohuq@github.com>
2021-10-04 02:17:36 +0300

    Task1: Pass comparator explicitly

--- src/main/java/Week2Task1.java
+++ src/main/java/Week2Task1.java
@@ -19,7 +19,7 @@ public final class Week2Task1 {
         for (int i = 0; i < entries.length; i++) {
             entries[i] = new OlympiadEntry(in);
         }
-        mergeSort(entries);
+        mergeSort(entries, OlympiadEntry.byResult);
         for (OlympiadEntry entry : entries) {
             entry.println(out);
         }
@@ -50,24 +50,24 @@ public final class Week2Task1 {
                 .thenComparingInt((OlympiadEntry e) -> e.id);
     }
 
-    static void mergeSort(OlympiadEntry[] array) {
+    static void mergeSort(OlympiadEntry[] array, Comparator<OlympiadEntry> comparator) {
         OlympiadEntry[] buffer = new OlympiadEntry[array.length];
-        mergeSort(array, 0, array.length, buffer);
+        mergeSort(array, 0, array.length, buffer, comparator);
     }
 
-    static void mergeSort(OlympiadEntry[] array, int begin, int end, OlympiadEntry[] buffer) {
+    static void mergeSort(OlympiadEntry[] array, int begin, int end, OlympiadEntry[] buffer, Comparator<OlympiadEntry> comparator) {
         if (begin >= end - 1) {
             return;
         }
         int mid = begin + ((end - begin) >> 1);
-        mergeSort(array, begin, mid, buffer);
-        mergeSort(array, mid, end, buffer);
-        merge(array, begin, mid, end, buffer);
+        mergeSort(array, begin, mid, buffer, comparator);
+        mergeSort(array, mid, end, buffer, comparator);
+        merge(array, begin, mid, end, buffer, comparator);
     }
 
-    static void merge(OlympiadEntry[] array, int begin, int mid, int end, OlympiadEntry[] buffer) {
+    static void merge(OlympiadEntry[] array, int begin, int mid, int end, OlympiadEntry[] buffer, Comparator<OlympiadEntry> comparator) {
         for (int i = begin, j = mid, k = i; i < mid || j < end; ) {
-            if (j >= end || i < mid && OlympiadEntry.byResult.compare(array[i], array[j]) <= 0) {
+            if (j >= end || i < mid && comparator.compare(array[i], array[j]) <= 0) {
                 buffer[k++] = array[i++];
             } else {
                 buffer[k++] = array[j++];
