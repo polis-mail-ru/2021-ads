cc83b2892f8998844bf456b691bd987f3ca7d970
mariohuq <mariohuq@github.com>
2021-10-04 19:37:01 +0300

    Task5: solve, test

--- src/main/java/Week2Task5.java
+++ src/main/java/Week2Task5.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -14,7 +15,73 @@ public final class Week2Task5 {
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        final IntArraySet setA = new IntArraySet(in);
+        final IntArraySet setB = new IntArraySet(in);
+        out.println(setA.equals(setB) ? "YES" : "NO");
+    }
+
+    private static class IntArraySet {
+        private final int[] array;
+        private int length;
+
+        public IntArraySet(int capacity) {
+            array = new int[capacity];
+            length = 0;
+        }
+
+        public IntArraySet(FastScanner in) {
+            this(in.nextInt());
+            for (int i = 0; i < array.length; i++) {
+                add(in.nextInt());
+            }
+        }
+
+        public int length() {
+            return length;
+        }
+
+        public void add(int item) {
+            if (length >= array.length) {
+                // set is full
+                return;
+            }
+            final int position = lowerBound(0, length, item);
+            if (position >= length) {
+                array[length++] = item;
+                return;
+            }
+            if (array[position] == item) {
+                return;
+            }
+            System.arraycopy(array, position, array, position + 1, length - position);
+            array[position] = item;
+            length++;
+        }
+
+        public boolean equals(IntArraySet that) {
+            if (this == that) return true;
+            return length == that.length
+                    && Arrays.equals(array, 0, length, that.array, 0, that.length);
+        }
+
+        /**
+         * min i in [begin; begin+step) such that this.array[i] >= item;
+         * begin + step if no such i exists
+         */
+        private int lowerBound(int begin, int step, int item) {
+            if (step <= 0) {
+                return begin;
+            }
+
+            final int compare = Integer.compare(array[begin + step / 2], item);
+            if (compare == 0) {
+                return begin + step / 2;
+            }
+            if (compare < 0) {
+                return lowerBound(begin + step / 2 + 1, (step - 1) / 2, item);
+            }
+            return lowerBound(begin, step / 2, item);
+        }
     }
 
     private static class FastScanner {
