e06807ccb111c55b2221f080708b93d7800775c3
Vladislav Pochernin <vspochernin@gmail.com>
2021-10-10 18:36:27 +0300

    task3: in process

--- src/main/java/ru/mail/polis/ads/part3/vspochernin/task3/Main.java
+++ src/main/java/ru/mail/polis/ads/part3/vspochernin/task3/Main.java
@@ -12,72 +12,13 @@ import java.util.StringTokenizer;
  * Problem solution template.
  */
 public class Main {
+    private static final int[] maxHeapArray = new int[5000001];
+    private static final int[] minHeapArray = new int[5000001];
+    private static int maxHeapSize = 0;
+    private static int minHeapSize = 0;
+
     private Main() {
         // Should not be instantiated
-    }
-
-    private static class Heap {
-        private int[] heap;
-        private int size;
-        private final boolean isMaxHeap;
-        private final int capacity;
-
-        public Heap(int capacity, boolean isMaxHeap) {
-            heap = new int[capacity + 1];
-            this.capacity = capacity;
-            this.size = 0;
-            this.isMaxHeap = isMaxHeap;
-        }
-
-        private void swap(int i, int j) {
-            int temp = heap[i];
-            heap[i] = heap[j];
-            heap[j] = temp;
-        }
-
-        private void swim(int k) {
-            while (k > 1 && ((isMaxHeap && heap[k] > heap[k / 2]) || (!isMaxHeap && heap[k] < heap[k / 2]))) {
-                swap(k, k / 2);
-                k = k / 2;
-            }
-        }
-
-        private void sink(int k) {
-            while (2 * k <= size) {
-                int j = 2 * k; // Левый ребенок.
-                if (j < size && ((isMaxHeap && heap[j] < heap[j + 1]) || (!isMaxHeap && heap[j] > heap[j + 1]))) {
-                    j++; // Правый ребенок.
-                }
-                if (((isMaxHeap && heap[k] >= heap[j]) || (!isMaxHeap && heap[k] <= heap[j]))) {
-                    break; // Инвариант восстановился.
-                }
-                swap(k, j);
-                k = j;
-            }
-        }
-
-        public void insert(int x) {
-            heap[++size] = x;
-            swim(size);
-        }
-
-        public int extract() {
-            int max = heap[1];
-            swap(1, size--);
-            sink(1);
-            return max;
-        }
-
-        public int peek() {
-            if (size == 0) {
-                return 0;
-            }
-            return heap[1];
-        }
-
-        public int getSize() {
-            return size;
-        }
     }
 
     private static void swap(int[] a, int i, int j) {
@@ -86,34 +27,94 @@ public class Main {
         a[j] = temp;
     }
 
+    private static void maxHeapSwim(int k) {
+        while (k > 1 && maxHeapArray[k] > maxHeapArray[k / 2]) {
+            swap(maxHeapArray, k, k / 2);
+            k = k / 2;
+        }
+    }
+
+    private static void minHeapSwim(int k) {
+        while (k > 1 && minHeapArray[k] < minHeapArray[k / 2]) {
+            swap(minHeapArray, k, k / 2);
+            k = k / 2;
+        }
+    }
+
+    private static void maxHeapSink(int k) {
+        while (2 * k <= maxHeapSize) {
+            int j = 2 * k;
+            if (j < maxHeapSize && maxHeapArray[j] < maxHeapArray[j + 1]) {
+                j++;
+            }
+            if (maxHeapArray[k] >= maxHeapArray[j]) {
+                break;
+            }
+            swap(maxHeapArray, k, j);
+            k = j;
+        }
+    }
+    private static void minHeapSink(int k) {
+        while (2 * k <= minHeapSize) {
+            int j = 2 * k;
+            if (j < minHeapSize && minHeapArray[j] > minHeapArray[j + 1]) {
+                j++;
+            }
+            if (minHeapArray[k] <= minHeapArray[j]) {
+                break;
+            }
+            swap(minHeapArray, k, j);
+            k = j;
+        }
+    }
+
+    private static void maxHeapInsert(int x) {
+        maxHeapArray[++maxHeapSize] = x;
+        maxHeapSwim(maxHeapSize);
+    }
+    private static void minHeapInsert(int x) {
+        minHeapArray[++minHeapSize] = x;
+        minHeapSwim(minHeapSize);
+    }
+
+    private static int maxHeapExtract() {
+        int max = maxHeapArray[1];
+        swap(maxHeapArray, 1, maxHeapSize--);
+        maxHeapSink(1);
+        return max;
+    }
+    private static int minHeapExtract() {
+        int min = minHeapArray[1];
+        swap(minHeapArray, 1, minHeapSize--);
+        minHeapSink(1);
+        return min;
+    }
+
     private static void solve(final FastScanner in, final PrintWriter out) {
-        Heap maxHeap = new Heap(500000, true);
-        Heap minHeap = new Heap(500000, false);
-        boolean isSeparatedMedian = false;
+        boolean isSeparatedMedian = true;
         int n;
         int median = 0;
         int[] a = new int[3];
         Scanner newIn = new Scanner(System.in);
         if (newIn.hasNextInt()) {
             median = newIn.nextInt();
-            isSeparatedMedian = !isSeparatedMedian;
             System.out.println(median);
         }
         while (newIn.hasNextInt()) {
             n = newIn.nextInt();
             if (isSeparatedMedian) { // Медиана была отдельным элементом.
                 if (n > median) {
-                    minHeap.insert(n);
-                    maxHeap.insert(median);
+                    minHeapInsert(n);
+                    maxHeapInsert(median);
                 } else {
-                    minHeap.insert(median);
-                    maxHeap.insert(n);
+                    minHeapInsert(median);
+                    maxHeapInsert(n);
                 }
-                median = (maxHeap.peek() + minHeap.peek()) / 2;
+                median = (maxHeapArray[1] + minHeapArray[1]) / 2;
             } else { // Медиана была средним арифметическим.
-                a[0] = maxHeap.extract();
+                a[0] = maxHeapExtract();
                 a[1] = n;
-                a[2] = minHeap.extract();
+                a[2] = minHeapExtract();
                 if (a[0] > a[1]) {
                     swap(a, 0, 1);
                 }
@@ -123,25 +124,13 @@ public class Main {
                 if (a[0] > a[1]) {
                     swap(a, 0, 1);
                 }
-                maxHeap.insert(a[0]);
+                maxHeapInsert(a[0]);
                 median = a[1];
-                minHeap.insert(a[2]);
+                minHeapInsert(a[2]);
             }
             isSeparatedMedian = !isSeparatedMedian;
             System.out.println(median);
         }
-
-        /*int n = in.nextInt();
-        Heap heap = new Heap(n);
-        int command;
-        for (int i = 0; i < n; i++) {
-            command = in.nextInt();
-            if (command == 0) {
-                heap.insert(in.nextInt());
-            } else if (command == 1) {
-                System.out.println(heap.extract());
-            }
-        }*/
     }
 
     private static class FastScanner {
