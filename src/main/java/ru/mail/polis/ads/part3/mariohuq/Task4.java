a42d8e791f5ef095f8e07a2517ea0eabaf768fcd
mariohuq <mariohuq@github.com>
2021-10-12 17:31:06 +0300

    Task4: solve, tested

--- src/main/java/ru/mail/polis/ads/part3/mariohuq/Task4.java
+++ src/main/java/ru/mail/polis/ads/part3/mariohuq/Task4.java
@@ -16,7 +16,60 @@ public final class Task4 {
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int n = in.nextInt();
+        MaxHeap heap = new MaxHeap(n);
+        for (int i = 0; i < n; i++) {
+            heap.insert(in.nextInt());
+        }
+        for (int i = MaxHeap.ROOT_INDEX; i <= heap.length(); i++) {
+            out.println(heap.array[i]);
+        }
+    }
+
+    private static class MaxHeap {
+        private final int[] array;
+        private int last_index;
+        private static final Invariant invariant = (parent, child) -> parent >= child;
+
+        private final static int ROOT_INDEX = 1;
+
+        /**
+         * @param size maximum size of the heap
+         */
+        public MaxHeap(int size) {
+            array = new int[size + ROOT_INDEX];
+        }
+
+        public void insert(int x) {
+            array[++last_index] = x;
+            siftUp(last_index);
+        }
+
+        private void siftUp(int index) {
+            int k = index;
+            while (k > ROOT_INDEX && !invariant.holds(array[parent(k)], array[k])) {
+                swap(k, parent(k));
+                k = parent(k);
+            }
+        }
+
+        private void swap(int left_index, int right_index) {
+            int tmp = array[left_index];
+            array[left_index] = array[right_index];
+            array[right_index] = tmp;
+        }
+
+        private static int parent(int child_index) {
+            return child_index / 2;
+        }
+
+        public int length() {
+            return last_index - ROOT_INDEX + 1;
+        }
+
+        public interface Invariant {
+            boolean holds(int parent_value, int child_value);
+        }
     }
 
     private static class FastScanner {
