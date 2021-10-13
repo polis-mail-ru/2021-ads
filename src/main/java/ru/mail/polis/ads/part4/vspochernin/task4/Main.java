f6c71ef0f529d9bfcb8c7cf1313dab5870e0c33c
Vladislav Pochernin <vspochernin@gmail.com>
2021-10-13 17:23:10 +0300

    part4-task4: in process

---
+++ src/main/java/ru/mail/polis/ads/part4/vspochernin/task4/Main.java
@@ -0,0 +1,71 @@
+package ru.mail.polis.ads.part4.vspochernin.task4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * Problem solution template.
+ */
+public final class Main {
+    private Main() {
+        // Should not be instantiated
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int n = in.nextInt();
+        int[] c = new int [n + 2]; // Стоимость каждой ступеньки.
+        int[] d = new int[n + 2]; // Максимальная стоимость пути для каждой ступеньки.
+        for(int i = 0; i < n; i++) {
+            c[i + 1] = in.nextInt();
+        }
+        int k = in.nextInt();
+
+        d[0] = 0; // База.
+        // d(i) = max(d(i - 1), d(i - 2), ..., d(i - i)) + c(i).
+        for (int i = 1; i <= k; i++) {
+        }
+        // d(i) = max(d(i - 1), d(i - 2), ..., d(i - k)) + c(i).
+        for (int i = k + 1; i < n + 2; i++) {
+
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static PrintWriter createPrintWriterForLocalTests() {
+        return new PrintWriter(System.out, true);
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
