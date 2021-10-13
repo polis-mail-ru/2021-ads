aa3ebee6d982ec481706b12accab1e708d3ad28e
Vladislav Pochernin <vspochernin@gmail.com>
2021-10-13 12:40:17 +0300

    part4-task5: in process

---
+++ src/main/java/ru/mail/polis/ads/part4/vspochernin/task5/Main.java
@@ -0,0 +1,70 @@
+package ru.mail.polis.ads.part4.vspochernin.task5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * Problem solution template.
+ */
+public final class Main {
+    private Main() {
+        // Should not be instantiated
+    }
+
+    int countInv(int[] a, int i, int j) {
+        if (j - i <= 1) {
+            return 0;
+        }
+        int mid = (i + j) / 2;
+        return countInv(a, i, mid) +
+                countInv(a, mid + 1, j) +
+                countInv(a, i, j, mid);
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static PrintWriter createPrintWriterForLocalTests() {
+        return new PrintWriter(System.out, true);
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
