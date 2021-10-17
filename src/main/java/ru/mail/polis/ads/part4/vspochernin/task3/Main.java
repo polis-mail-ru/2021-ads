a36cadeadd6d157cec520105b5cb2f669b08002b
Vladislav Pochernin <vspochernin@gmail.com>
2021-10-17 18:03:43 +0300

    part4-task3: done

---
+++ src/main/java/ru/mail/polis/ads/part4/vspochernin/task3/Main.java
@@ -0,0 +1,79 @@
+package ru.mail.polis.ads.part4.vspochernin.task3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * Problem solution template.
+ * https://www.e-olymp.com/ru/submissions/9563631
+ */
+public final class Main {
+    private Main() {
+        // Should not be instantiated
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int n = in.nextInt();
+        int[] a = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            a[i] = in.nextInt();
+        }
+        int m = in.nextInt();
+        int[] b = new int[m + 1];
+        for (int i = 1; i <= m; i++) {
+            b[i] = in.nextInt();
+        }
+
+        int[][] d = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (a[i] == b[j]) {
+                    d[i][j] = d[i - 1][j - 1] + 1;
+                } else {
+                    d[i][j] = Math.max(d[i - 1][j], d[i][j - 1]);
+                }
+            }
+        }
+
+        out.println(d[n][m]);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static PrintWriter createPrintWriterForLocalTests() {
+        return new PrintWriter(System.out, true);
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
