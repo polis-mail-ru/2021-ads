0eefd44c8cd713aee4b4017a7c64baf309b7e2b9
mariohuq <mariohuq@github.com>
2021-10-18 21:37:11 +0300

    Task5: solve, tested

--- src/main/java/ru/mail/polis/ads/part4/mariohuq/Task5.java
+++ src/main/java/ru/mail/polis/ads/part4/mariohuq/Task5.java
@@ -5,12 +5,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
  * 4261. Количество инверсий
  * <p>
- * https://www.e-olymp.com/ru/submissions/XXXXX
+ * https://www.e-olymp.com/ru/submissions/9576491
  */
 public final class Task5 {
     private Task5() {
@@ -18,7 +19,41 @@ public final class Task5 {
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int[] input = new int[in.nextInt()];
+        Arrays.setAll(input, i -> in.nextInt());
+        out.println(countInversions(input));
+    }
+
+    private static int countInversions(int[] array) {
+        int[] buffer = new int[array.length];
+        return countInversions(array, 0, array.length, buffer);
+    }
+
+    private static int countInversions(int[] array, int begin, int end, int[] buffer) {
+        if (end - begin <= 1) {
+            return 0;
+        }
+        int mid = begin + (end - begin) / 2;
+        return countInversions(array, begin, mid, buffer)
+                + countInversions(array, mid, end, buffer)
+                + countSplitInversions(array, begin, end, buffer);
+    }
+
+    private static int countSplitInversions(int[] array, int begin, int end, int[] buffer) {
+        int mid = begin + (end - begin) / 2;
+        int inversions = 0;
+        for (int i = begin, j = mid, k = i; i < mid || j < end; ) {
+            if (j >= end || i < mid && array[i] <= array[j]) {
+                buffer[k++] = array[i++];
+                inversions += j - mid;
+            } else {
+                buffer[k++] = array[j++];
+            }
+        }
+        if (end - begin >= 0) {
+            System.arraycopy(buffer, begin, array, begin, end - begin);
+        }
+        return inversions;
     }
 
     private static class FastScanner {
