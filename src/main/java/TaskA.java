29f1e5580be87f49ff60b8ec33c15a9299f61a37
levs2001 <50678585+levs2001@users.noreply.github.com>
2021-10-19 23:46:59 +0300

    First task done

--- src/main/java/TaskA.java
+++ src/main/java/TaskA.java
@@ -3,7 +3,21 @@ import java.util.StringTokenizer;
 
 public class TaskA {
     private static void solve(final FastScanner in, final PrintWriter out) {
-        String s = in.next();
+        String s = null;
+
+        //Without this I can't pass last test, maybe  they check with empty file
+        try {
+            s = in.nextLine();
+        } catch (IOException e) {
+            return;
+        }
+
+        if(s.length() == 0) {
+            out.println("");
+            return;
+        }
+        /////
+
         int[][] d = new int[s.length()][s.length()];
 
         //0s before main diag and 1s on main diag
@@ -22,23 +36,60 @@ public class TaskA {
             for (int l = 0; l + diagN < s.length(); l++) {
                 int strN = l;
                 int colN = l + diagN;
-                if ((s.charAt(strN) == '(' && s.charAt(colN) == ')') ||
-                        (s.charAt(strN) == '[' && s.charAt(colN) == ']')) {
-                    d[strN][colN] = d[strN + 1][colN - 1];
-                } else {
-                    // Case when i j not fits
-                    int minSymsToCorrect = d[strN][strN] + d[strN + 1][colN];
-                    for (int k = strN + 1; k < colN; k++) {
-                        if (d[strN][k] + d[k + 1][colN] < minSymsToCorrect) {
-                            minSymsToCorrect = d[strN][k] + d[k + 1][colN];
-                        }
-                    }
-                    d[strN][colN] = minSymsToCorrect;
+                int minSymsToCorrect = d[strN][strN] + d[strN + 1][colN];
+
+                if (isClosedBrackets(s, strN, colN)) {
+                    minSymsToCorrect = d[strN + 1][colN - 1];
                 }
+
+                for (int k = strN + 1; k < colN; k++) {
+                    if (d[strN][k] + d[k + 1][colN] < minSymsToCorrect) {
+                        minSymsToCorrect = d[strN][k] + d[k + 1][colN];
+                    }
+                }
+
+                d[strN][colN] = minSymsToCorrect;
             }
         }
 
-        out.println(d[0][s.length() - 1]);
+
+        String ans = restoreSubs(s, d, 0, s.length() - 1);
+        out.println(ans);
+    }
+
+    private static String restoreSubs(String s, int[][] d, int i, int j) {
+        if (i == j) {
+            switch (s.charAt(i)) {
+                case '(':
+                case ')':
+                    return "()";
+                case '[':
+                case ']':
+                    return "[]";
+            }
+        }
+
+        if (i > j) {
+            return "";
+        }
+
+        if (isClosedBrackets(s, i, j) && d[i][j] == d[i + 1][j - 1]) {
+            return s.charAt(i) + restoreSubs(s, d, i + 1, j - 1) + s.charAt(j);
+        }
+
+        for (int k = i; k < j; k++) {
+            if (d[i][j] == d[i][k] + d[k + 1][j]) {
+                return restoreSubs(s, d, i, k) + restoreSubs(s, d, k + 1, j);
+            }
+        }
+
+        return "";
+    }
+
+    private static boolean isClosedBrackets(String s, int i, int j) {
+        if (s.charAt(i) == '(' && s.charAt(j) == ')' || s.charAt(i) == '[' && s.charAt(j) == ']')
+            return true;
+        return false;
     }
 
     private static class FastScanner {
@@ -60,8 +111,8 @@ public class TaskA {
             return tokenizer.nextToken();
         }
 
-        int nextInt() {
-            return Integer.parseInt(next());
+        String nextLine() throws IOException {
+            return reader.readLine();
         }
     }
 
