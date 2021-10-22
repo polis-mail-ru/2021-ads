270dc90e7df19637e35348a6dc66fd4a76f77ae1
donebd <dinil.bd@mail.ru>
2021-10-22 03:07:41 +0300

    Почему то код задачи про мышку исчез при коммите

--- src/main/java/ru/mail/polis/ads/MouseBeReachTask.java
+++ src/main/java/ru/mail/polis/ads/MouseBeReachTask.java
@@ -10,8 +10,52 @@ import java.util.StringTokenizer;
 public class MouseBeReachTask {
 
     private static void solve(final FastScanner in, final PrintWriter out) {
+        int m = in.nextInt();
+        int n = in.nextInt();
 
-        
+        int[][] data = new int[m + 1][n + 1];
+        for (int row = 0; row < m; row++) {
+            for (int column = 1; column <= n; column++) {
+                //[m-row]for convenience
+                data[m - row][column] = in.nextInt();
+            }
+        }
+        int[][] dynamicMagic = new int[m + 1][n + 1];
+        boolean[][] fromBack = new boolean[m][n];//true = F
+        for (int row = 1; row <= m; row++) {
+            for (int column = 1; column <= n; column++) {
+                dynamicMagic[row][column] = data[row][column];
+                if (dynamicMagic[row - 1][column] > dynamicMagic[row][column - 1]) {
+                    dynamicMagic[row][column] += dynamicMagic[row - 1][column];
+                    fromBack[row - 1][column - 1] = true;
+                    continue;
+                }
+                dynamicMagic[row][column] += dynamicMagic[row][column - 1];
+            }
+        }
+        StringBuilder answer = new StringBuilder();
+        int i = m - 1;
+        int j = n - 1;
+        while (i != 0 || j != 0) {
+            if (i != 0 && j != 0) {
+                if (fromBack[i][j]) {
+                    i--;
+                    answer.append('F');
+                } else {
+                    j--;
+                    answer.append('R');
+                }
+                continue;
+            }
+            if (i != 0) {
+                i--;
+                answer.append('F');
+                continue;
+            }
+            j--;
+            answer.append('R');
+        }
+        out.println(answer.reverse());
     }
 
     private static class FastScanner {
