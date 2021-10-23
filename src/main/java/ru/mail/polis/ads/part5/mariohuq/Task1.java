c97773c3e5316ec5c16531cde88fbaba42ac60e7
mariohuq <mariohuq@github.com>
2021-10-23 15:46:30 +0300

    Task1: solve, tested

--- src/main/java/ru/mail/polis/ads/part5/mariohuq/Task1.java
+++ src/main/java/ru/mail/polis/ads/part5/mariohuq/Task1.java
@@ -6,19 +6,51 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
+import java.util.function.DoubleUnaryOperator;
 
 /**
  * 3968. Квадратный корень
  * <p>
- * https://www.e-olymp.com/ru/submissions/XXXXXXX
+ * https://www.e-olymp.com/ru/submissions/9626769
  */
 public final class Task1 {
+    private static final double EPSILON = 1e-6;
+    // ceiling of positive solution of x^2 + sqrt(x) = 10^10
+    public static final double MAX_T = 1e5;
+    public static final double MIN_T = 0;
+
     private Task1() {
         // Should not be instantiated
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        double c = in.nextDouble();
+        double x = solution(MIN_T, MAX_T, t -> t * t + Math.sqrt(t), c);
+        out.println(x);
+    }
+
+    /**
+     * Preconditions:
+     *     maxT >= minT
+     *     for all t1, t2 from [minT; maxT]:
+     *         if t1 <= t2 then leftHandSide.applyAsDouble(t1) <= leftHandSide.applyAsDouble(t2)
+     *         (in other words, leftHandSide is non-decreasing on [minT; maxT])
+     *     leftHandSide.applyAsDouble(minT) <= rightHandSide
+     *     leftHandSide.applyAsDouble(maxT) >= rightHandSide
+     */
+    private static double solution(double minT, double maxT, DoubleUnaryOperator leftHandSide, double rightHandSide) {
+        double midT = (maxT + minT) / 2;
+        if (maxT - minT <= EPSILON) {
+            return midT;
+        }
+        int compare = Double.compare(leftHandSide.applyAsDouble(midT), rightHandSide);
+        if (compare == 0) {
+            return midT;
+        }
+        if (compare > 0) {
+            return solution(minT, midT, leftHandSide, rightHandSide);
+        }
+        return solution(midT, maxT, leftHandSide, rightHandSide);
     }
 
     private static class FastScanner {
@@ -42,6 +74,10 @@ public final class Task1 {
 
         int nextInt() {
             return Integer.parseInt(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
         }
     }
 
