be49e07d6a858adc4a5ff2ff7448322c107c1f82
CORADEAD <Nahnupa2002>
2021-10-31 21:37:30 +0300

    Galeev HW 5

--- src/main/java/ru/mail/polis/ads/TaskE.java
+++ src/main/java/ru/mail/polis/ads/TaskE.java
@@ -6,13 +6,50 @@ import java.util.StringTokenizer;
 /**
  * Problem solution template.
  */
-public final class Main {
-    private Main() {
+public final class TaskE {
+    private TaskE() {
         // Should not be instantiated
     }
 
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int arrSize = in.nextInt();
+        int[] array = new int[arrSize];
+        for (int i = 0; i < arrSize; i++) {
+            array[i] = i + 1;
+        }
+        while (true) {
+            for (int i = 0; i < arrSize; i++) {
+                out.print(array[i] + " ");
+            }
+            out.println();
+            // Index Of First Growing Pair
+            int iGP;
+            for (iGP = arrSize - 2; iGP >= 0; iGP--) {
+                if (array[iGP + 1] > array[iGP]) {
+                    break;
+                }
+            }
+            if (iGP == -1) {
+                return;
+            }
+            // Swap iGP with first element, bigger than iGP
+            for (int k = arrSize - 1; k > iGP; k--) {
+                if (array[k] > array[iGP]) {
+                    swap(array, k, iGP);
+                    break;
+                }
+            }
+            // reverse
+            for (int i1 = iGP + 1, i2 = arrSize - 1; i1 < i2; i1++, i2--) {
+                swap(array, i1, i2);
+            }
+        }
     }
 
     private static class FastScanner {
