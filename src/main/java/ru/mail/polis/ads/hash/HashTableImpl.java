75bcfc633340561ac04d97b1da77c9e6e346e3b3
Egor Shakh <egor.shakh@corp.mail.ru>
2021-11-15 19:23:49 +0300

    dumb impl

--- src/main/java/ru/mail/polis/ads/hash/HashTableImpl.java
+++ src/main/java/ru/mail/polis/ads/hash/HashTableImpl.java
@@ -1,35 +1,40 @@
 package ru.mail.polis.ads.hash;
 
+import java.util.HashMap;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class HashTableImpl<Key, Value> implements HashTable<Key, Value> {
 
+    private HashMap<Key, Value> map;
+
     public HashTableImpl() {
+        map = new HashMap<>();
     }
 
     @Override
     public @Nullable Value get(@NotNull Key key) {
-        throw new UnsupportedOperationException();
+        return map.get(key);
     }
 
     @Override
     public void put(@NotNull Key key, @NotNull Value value) {
-        throw new UnsupportedOperationException();
+        map.put(key,value);
     }
 
     @Override
     public @Nullable Value remove(@NotNull Key key) {
-        throw new UnsupportedOperationException();
+        map.remove(key);
     }
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return true;
+        return map.isEmpty();
     }
 }
