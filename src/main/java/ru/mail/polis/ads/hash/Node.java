462350d8ea40ebde7561d270ead399e0105377d6
Komissarov Gleb <gl.k@aol.com>
2021-11-26 13:31:32 +0300

    Reworked resize

---
+++ src/main/java/ru/mail/polis/ads/hash/Node.java
@@ -0,0 +1,32 @@
+package ru.mail.polis.ads.hash;
+
+public class Node<K, V> {
+    private final K key;
+    private V value;
+    private Node<K, V> nextNode = null;
+
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public Node<K, V> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<K, V> node) {
+        nextNode = node;
+    }
+}
