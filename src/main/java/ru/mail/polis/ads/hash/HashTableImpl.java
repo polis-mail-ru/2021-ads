2d65a71c7425d627b317c092c2c9f3ae158fe644
done_bd <dinil.bd@mail.ru>
2021-11-29 19:58:32 +0300

    Fixes

--- src/main/java/ru/mail/polis/ads/hash/HashTableImpl.java
+++ src/main/java/ru/mail/polis/ads/hash/HashTableImpl.java
@@ -10,7 +10,7 @@ public class HashTableImpl<Key, Value> implements HashTable<Key, Value> {
     private static final double LOAD_FACTOR = 0.75;
     private static final int INITIAL_CAPACITY = 29;
 
-    private List<Map<Integer, Pair<Key, Value>>> hashTable = new ArrayList<>(INITIAL_CAPACITY);
+    private List<List<Pair<Key, Value>>> hashTable = new ArrayList<>(INITIAL_CAPACITY);
     private int currentSize;
     private int tableSize = INITIAL_CAPACITY;
 
@@ -22,9 +22,9 @@ public class HashTableImpl<Key, Value> implements HashTable<Key, Value> {
     @Override
     public @Nullable Value get(@NotNull Key key) {
         int hash = key.hashCode() & Integer.MAX_VALUE;
-        Map<Integer, Pair<Key, Value>> link = hashTable.get(hash % tableSize);
-        if (link != null) {
-            Pair<Key, Value> get = link.get(hash);
+        List<Pair<Key, Value>> list = hashTable.get(hash % tableSize);
+        if (list != null) {
+            Pair<Key, Value> get = getByHash(list, hash);
             while (get != null) {
                 if (get.key.equals(key)) {
                     return get.value;
@@ -44,8 +44,8 @@ public class HashTableImpl<Key, Value> implements HashTable<Key, Value> {
     public @Nullable Value remove(@NotNull Key key) {
         int hash = key.hashCode() & Integer.MAX_VALUE;
         int index = hash % tableSize;
-        Map<Integer, Pair<Key, Value>> link = hashTable.get(index);
-        Pair<Key, Value> get = (link != null) ? link.get(hash) : null;
+        List<Pair<Key, Value>> list = hashTable.get(index);
+        Pair<Key, Value> get = (list != null) ? getByHash(list, hash) : null;
         Pair<Key, Value> parent = get;
         while (get != null && !get.key.equals(key)) {
             parent = get;
@@ -58,8 +58,8 @@ public class HashTableImpl<Key, Value> implements HashTable<Key, Value> {
             return null;
         }
         if (parent == get) {
-            if (parent.cloneByHash == null) {
-                Pair<Key, Value> deleted = link.remove(hash);
+            if (get.cloneByHash == null) {
+                Pair<Key, Value> deleted = removeByHash(list, hash);
                 currentSize--;
                 return deleted.value;
             }
@@ -68,7 +68,7 @@ public class HashTableImpl<Key, Value> implements HashTable<Key, Value> {
             get.key = get.cloneByHash.key;
             get.cloneByHash = get.cloneByHash.cloneByHash;
             currentSize--;
-            return get.value;
+            return deleted;
         }
         parent.cloneByHash = get.cloneByHash;
         currentSize--;
@@ -88,16 +88,16 @@ public class HashTableImpl<Key, Value> implements HashTable<Key, Value> {
     private void putImpl(@NotNull Key key, @NotNull Value value, boolean countSize) {
         int hash = key.hashCode() & Integer.MAX_VALUE;
         int index = hash % tableSize;
-        Map<Integer, Pair<Key, Value>> link = hashTable.get(index);
-        if (link == null) {
-            hashTable.set(index, new HashMap<>());
-            hashTable.get(index).put(hash, new Pair<>(key, value));
+        List<Pair<Key, Value>> list = hashTable.get(index);
+        if (list == null) {
+            hashTable.set(index, new LinkedList<>());
+            hashTable.get(index).add(new Pair<>(key, value, hash));
             if (countSize) {
                 currentSize++;
             }
             return;
         }
-        Pair<Key, Value> get = link.get(hash);
+        Pair<Key, Value> get = getByHash(list, hash);
         if (get != null) {
             if (get.key.equals(key)) {
                 get.value = value;
@@ -111,13 +111,13 @@ public class HashTableImpl<Key, Value> implements HashTable<Key, Value> {
                 }
                 get = get.cloneByHash;
             }
-            get.cloneByHash = new Pair<>(key, value);
+            get.cloneByHash = new Pair<>(key, value, hash);
             if (countSize) {
                 currentSize++;
             }
             return;
         }
-        link.put(hash, new Pair<>(key, value));
+        list.add(new Pair<>(key, value, hash));
         if (countSize) {
             currentSize++;
         }
@@ -134,15 +134,15 @@ public class HashTableImpl<Key, Value> implements HashTable<Key, Value> {
         }
         int oldSize = tableSize;
         tableSize = oldSize * 2;
-        List<Map<Integer, Pair<Key, Value>>> oldHash = hashTable;
+        List<List<Pair<Key, Value>>> oldHash = hashTable;
         hashTable = new ArrayList<>(tableSize);
         initArray();
 
-        for (Map<Integer, Pair<Key, Value>> link : oldHash) {
-            if (link == null) {
+        for (List<Pair<Key, Value>> list : oldHash) {
+            if (list == null) {
                 continue;
             }
-            for (Pair<Key, Value> element : link.values()) {
+            for (Pair<Key, Value> element : list) {
                 Pair<Key, Value> get = element;
                 putImpl(element.key, element.value, false);
                 while (get.cloneByHash != null) {
@@ -159,14 +159,35 @@ public class HashTableImpl<Key, Value> implements HashTable<Key, Value> {
         }
     }
 
+    private Pair<Key, Value> getByHash(List<Pair<Key, Value>> list, int hash){
+        for (Pair<Key, Value> pair: list) {
+            if (pair.valueHashcode == hash) {
+                return pair;
+            }
+        }
+        return null;
+    }
+
+    private Pair<Key, Value> removeByHash(List<Pair<Key, Value>> list, int hash){
+        for (Pair<Key, Value> pair: list) {
+            if (pair.valueHashcode == hash) {
+                list.remove(pair);
+                return pair;
+            }
+        }
+        return null;
+    }
+
     private static class Pair<Key, Value> {
         Key key;
         Value value;
         Pair<Key, Value> cloneByHash;
+        int valueHashcode;
 
-        Pair(Key key, Value value) {
+        Pair(Key key, Value value, int hashcode) {
             this.key = key;
             this.value = value;
+            valueHashcode = hashcode;
         }
     }
 }
