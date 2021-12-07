a797304413d01b3fe503877609446a7f7323fee3
mariohuq <mariohuq@github.com>
2021-12-07 17:41:49 +0300

    Task1..4: template

---
+++ src/main/java/ru/mail/polis/ads/part10/mariohuq/Task1.java
@@ -0,0 +1,54 @@
+package ru.mail.polis.ads.part10.mariohuq;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * <a href="https://www.eolymp.com/ru/problems/3835">3835. Минимальный каркас</a>
+ * <p>
+ * <a href="https://www.eolymp.com/ru/submissions/">E-olymp submission</a>
+ */
+public final class Task1 {
+    private Task1() {
+        // Should not be instantiated
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        // Write me
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static PrintWriter createPrintWriterForLocalTests() {
+        return new PrintWriter(System.out, true);
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
