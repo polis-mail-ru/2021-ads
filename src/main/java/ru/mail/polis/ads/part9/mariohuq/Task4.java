aad0c0e9dbd99a74e461388d8cb8eb7efa20c245
mariohuq <mariohuq@github.com>
2021-12-07 03:37:19 +0300

    Task4: solve, tested

--- src/main/java/ru/mail/polis/ads/part9/mariohuq/Task4.java
+++ src/main/java/ru/mail/polis/ads/part9/mariohuq/Task4.java
@@ -1,12 +1,12 @@
 package ru.mail.polis.ads.part9.mariohuq;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
  * <a href="https://www.e-olymp.com/ru/problems/1947">1947. Конденсация графа</a>
  * <p>
- * <a href="https://www.eolymp.com/ru/submissions">E-olymp submission</a>
+ * <a href="https://www.eolymp.com/ru/submissions/10062314">E-olymp submission</a>
  */
 public class Task4 {
     private Task4() {
@@ -14,7 +14,131 @@ public class Task4 {
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int nVertices = in.nextInt();
+        int mEdges = in.nextInt();
+        Graph g = new Graph(nVertices);
+        for (int i = 0; i < mEdges; i++) {
+            int from = in.nextInt() - 1;
+            int to = in.nextInt() - 1;
+            g.addArrow(from, to);
+        }
+        out.println(new EdgesInCondensedCount(g).get());
+    }
+
+    private static class EdgesInCondensedCount {
+        private final Graph graph;
+        int[] topologicalOrder;
+        BitSet visited;
+        int orderIndex;
+        Map<Integer, Integer> vertexToComponent;
+        List<List<Integer>> components;
+        int iComponent;
+
+        EdgesInCondensedCount(Graph graph) {
+            this.graph = graph;
+        }
+
+        public int get() {
+            topologicalOrder = new int[graph.verticesSize()];
+            visited = new BitSet(graph.verticesSize());
+            orderIndex = topologicalOrder.length - 1;
+            for (int v = 0; v < topologicalOrder.length; v++) {
+                if (visited.get(v)) {
+                    continue;
+                }
+                dfsTopological(v);
+            }
+            vertexToComponent = new HashMap<>();
+            components = new ArrayList<>();
+            visited.clear();
+            for (int v : topologicalOrder) {
+                if (visited.get(v)) {
+                    continue;
+                }
+                components.add(new ArrayList<>());
+                dfsComponent(v);
+                iComponent++;
+            }
+            Graph g = new Graph(iComponent);
+            int edges = 0;
+            // or direct?
+            for (int i = iComponent - 1; i >= 0; i--) {
+                List<Integer> component = components.get(i);
+                for (int v : component) {
+                    for (int u : graph.getAdjacents(v)) {
+                        if (g.addArrow(vertexToComponent.get(v), vertexToComponent.get(u))) {
+                            edges++;
+                        }
+                    }
+                }
+            }
+            return edges;
+        }
+
+        private void dfsTopological(int vertex) {
+            visited.set(vertex);
+            for (int v : graph.getAdjacents(vertex)) {
+                if (visited.get(v)) {
+                    continue;
+                }
+                dfsTopological(v);
+            }
+            topologicalOrder[orderIndex--] = vertex;
+        }
+
+        private void dfsComponent(int vertex) {
+            visited.set(vertex);
+            vertexToComponent.put(vertex, iComponent);
+            components.get(iComponent).add(vertex);
+            for (int v : graph.getAdjacentsTransposed(vertex)) {
+                if (visited.get(v)) {
+                    continue;
+                }
+                dfsComponent(v);
+            }
+        }
+    }
+
+    private static class Graph {
+        final List<Set<Integer>> adjacencyList;
+        final List<Set<Integer>> transposedList;
+
+        public Graph(int verticesCount) {
+            adjacencyList = new ArrayList<>(verticesCount);
+            for (int i = 0; i < verticesCount; i++) {
+                adjacencyList.add(new HashSet<>());
+            }
+            transposedList = new ArrayList<>(verticesCount);
+            for (int i = 0; i < verticesCount; i++) {
+                transposedList.add(new HashSet<>());
+            }
+        }
+
+        public boolean addArrow(int from, int to) {
+            if (from == to) {
+                return false;
+            }
+            boolean added = adjacencyList.get(from).add(to);
+            transposedList.get(to).add(from);
+            return added;
+        }
+
+        public void addEdge(int from, int to) {
+            addArrow(from, to);
+            addArrow(to, from);
+        }
+
+        public Set<Integer> getAdjacents(int vertex) {
+            return adjacencyList.get(vertex);
+        }
+
+        public Set<Integer> getAdjacentsTransposed(int vertex) {
+            return transposedList.get(vertex);
+        }
+
+        public int verticesSize() {
+            return adjacencyList.size();
+        }
     }
 
     private static class FastScanner {
