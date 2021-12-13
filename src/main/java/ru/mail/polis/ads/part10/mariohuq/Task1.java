d12d0c0f3c39f3d25e00d694712a9fa5d89ea110
mariohuq <mariohuq@github.com>
2021-12-13 21:00:27 +0300

    Task1: solve, tested

--- src/main/java/ru/mail/polis/ads/part10/mariohuq/Task1.java
+++ src/main/java/ru/mail/polis/ads/part10/mariohuq/Task1.java
@@ -1,12 +1,13 @@
 package ru.mail.polis.ads.part10.mariohuq;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.IntStream;
 
 /**
  * <a href="https://www.eolymp.com/ru/problems/3835">3835. Минимальный каркас</a>
  * <p>
- * <a href="https://www.eolymp.com/ru/submissions/">E-olymp submission</a>
+ * <a href="https://www.eolymp.com/ru/submissions/10118438">E-olymp submission</a>
  */
 public final class Task1 {
     private Task1() {
@@ -14,7 +15,79 @@ public final class Task1 {
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int nVertices = in.nextInt();
+        int mEdges = in.nextInt();
+        Graph g = new Graph(mEdges, nVertices);
+        for (int i = 0; i < mEdges; i++) {
+            g.addEdge(in.nextInt() - 1, in.nextInt() - 1, in.nextInt());
+        }
+        g.sortEdges();
+        out.println(kruskalMstWeight(g));
+    }
+
+    private static int kruskalMstWeight(Graph g) {
+        final int[] f = IntStream.range(0, g.vertices()).toArray();
+        int result = 0;
+        int components = f.length;
+        for (Edge edge : g.edges()) {
+            if (f[edge.from] == f[edge.to]) {
+                continue;
+            }
+            result += edge.weight;
+            replace(f, f[edge.from], f[edge.to]);
+            components--;
+            if (components <= 1) {
+                break;
+            }
+        }
+        return result;
+    }
+
+    private static void replace(int[] f, int old, int new1) {
+        for (int i = 0; i < f.length; i++) {
+            if (f[i] == old) {
+                f[i] = new1;
+            }
+        }
+    }
+
+    private static class Graph {
+        private final Edge[] edges;
+        private int edgesCount;
+        private final int verticesCount;
+
+        public Graph(int edgesCount, int verticesCount) {
+            edges = new Edge[edgesCount];
+            this.verticesCount = verticesCount;
+        }
+
+        public void addEdge(int from, int to, int weight) {
+            edges[edgesCount++] = new Edge(from, to, weight);
+        }
+
+        public Edge[] edges() {
+            return edges;
+        }
+
+        public void sortEdges() {
+            Arrays.sort(edges, Comparator.comparing(e -> e.weight));
+        }
+
+        public int vertices() {
+            return verticesCount;
+        }
+    }
+
+    private static class Edge {
+        public final int from;
+        public final int to;
+        public final int weight;
+
+        public Edge(int from, int to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
     }
 
     private static class FastScanner {
